865d096d6035b83be112ec6431aa57ce67c7b447
schm00g <hk8igureu@relay.firefox.com>
2022-05-14 21:14:32 +0100

    use struct

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,5 @@ struct Cli {
 }
 
 fn main() {
-    let pattern = std::env::args().nth(1).expect("no pattern given");
-    let path = std::env::args().nth(2).expect("no path given");
+    let args = Cli::parse();
 }
