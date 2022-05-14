45088a20401c427a4e1272df902719838f957ee9
schm00g <hk8igureu@relay.firefox.com>
2022-05-14 21:50:24 +0100

    get context

--- src/main.rs
+++ src/main.rs
@@ -9,14 +9,15 @@ struct Cli {
     path: std::path::PathBuf,
 }
 
-fn main() {
-    let args = Cli::parse();
-    let content = std::fs::read_to_string(&args.path)
-        .expect("could not read file");
+#[derive(Debug)]
+struct CustomError(String);
 
-    for line in content.lines() {
-        if line.contains(&args.pattern) {
-            println!("{}", line);
-        }
-    }
+use anyhow::{Context, Result};
+
+fn main() -> Result<()> {
+    let path = "test.txt";
+    let content = std::fs::read_to_string(path)
+        .with_context(|| format!("could not read file `{}`", path))?;
+    println!("file content: {}", content);
+    Ok(())
 }
